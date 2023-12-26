2755837b169d7d52c0416d163f185141685db6ad
ViniciuMota <vinimota44@gmail.com>
2023-12-26 10:15:56 -0300

    convertendo para Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
+import { ThemeProvider } from 'styled-components'
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
-import './global.css'
+import EstiloGlobal, { Container } from './styles'
+import temaLight from './thema/light'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={temaLight}>
+      <EstiloGlobal />
       <Header />
       <Hero />
-      <div className="container">
+      <Container>
         <ListaVagas />
-      </div>
-    </>
+      </Container>
+    </ThemeProvider>
   )
 }
 
